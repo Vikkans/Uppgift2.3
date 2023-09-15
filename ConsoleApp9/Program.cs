9eca0a09d14722e8c75c1d8ca684d6f63adc48cb
Vikkans <Jakobsenviktor6@gmail.com>
2023-09-15 12:50:23 +0200

    Add project files.

---
+++ ConsoleApp9/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace ConsoleApp9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Hur många dagar ska ni köra bilen?");
+            string x = Console.ReadLine();
+            double z = double.Parse(x);
+
+            Console.WriteLine("Hur många kilometer ska ni köra bilen?");
+            string l = Console.ReadLine();
+            double y = double.Parse(l);
+
+            double sum = 300 + y + 500*z -500;
+
+            Console.WriteLine("Detta kommer att kosta dig " + sum + " kr");
+            Console.ReadKey();
+       
+
+
+
+        }
+
+
+
+    }
+
+
+
+}
